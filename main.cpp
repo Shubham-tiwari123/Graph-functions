38553a713278050b2d8764b5a9bfddada51f350f
Shubham-tiwari123 <st540362@gmail.com>
2018-03-22 22:31:05 +0530

    main file

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Graph.h"
+using namespace std;
+int main(){
+    Graph g;
+    g.undirectedGraph();
+    g.displayGraph();
+    g.directedGraph();
+    g.displayGraph();
+}
+
